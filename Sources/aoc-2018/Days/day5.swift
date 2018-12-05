728e10b37ee38f130fda02d912bf1fc3ece301a2
Simon Kågedal Reimer <skagedal@gmail.com>
2018-12-05 08:55:10 +0100

    Add 5B

--- Sources/aoc-2018/Days/day5.swift
+++ Sources/aoc-2018/Days/day5.swift
@@ -1,13 +1,31 @@
 import Foundation
 
+private let a: UInt8 = 97
+private let z: UInt8 = 122
+private let caseBit: UInt8 = 32
+
 func day5() throws {
     let data = try Data(file: .day5)
     
-    let a = data.reducingPolymers().count
-    print(a)
+    print(data.reducingPolymers().count)
+    print(data.shortestPolymer())
 }
 
-private extension Data {
+private extension UInt8 {
+    func reacts(with other: UInt8) -> Bool {
+        return self ^ other == caseBit
+    }
+}
+
+private extension Sequence where Element == UInt8 {
+    func shortestPolymer() -> Int {
+        return (a...z).map({
+            self.skippingCaseInsensitive(lowerCasedAscii: $0)
+                .reducingPolymers()
+                .count
+        }).min(by: <)!
+    }
+    
     func reducingPolymers() -> Data {
         return reduce(into: Data()) { result, polymer in
             if let previous = result.last, polymer.reacts(with: previous) {
@@ -17,10 +35,8 @@ private extension Data {
             }
         }
     }
-}
-
-private extension UInt8 {
-    func reacts(with other: UInt8) -> Bool {
-        return self ^ other == 32
+    
+    func skippingCaseInsensitive(lowerCasedAscii: UInt8) -> LazyFilterSequence<Self> {
+        return lazy.filter({ $0 | caseBit != lowerCasedAscii })
     }
 }
