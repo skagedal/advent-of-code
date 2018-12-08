7b70f62b8e6607a2d17c137616555bb1d1f7c3df
Simon Kågedal Reimer <skagedal@gmail.com>
2018-12-08 23:33:52 +0100

    Optimize MD5 checking code

--- Sources/aoc-2018/Year2015/Year2015-Day04.swift
+++ Sources/aoc-2018/Year2015/Year2015-Day04.swift
@@ -6,25 +6,46 @@ extension Year2015 {
         let day = 4
 
         func answerToFirstPart(_ data: Data) throws -> String {
-            let prefix = String(data: data, encoding: .utf8)!
-            let fiveZeros = "00000"
-            for int in 0...Int.max {
-                let string = "\(prefix)\(int)"
-                let md5 = MD5(string: string).hexEncodedString()
-                if md5.prefix(5) == fiveZeros {
-                    return "\(int)"
-                }
-            }
-            throw AdventError.noAnswerFound
+            return findMD5(messagePrefix: data, matching: hasFiveZeroNibblePrefix).toString
         }
     
         func answerToSecondPart(_ data: Data) throws -> String {
-            throw AdventError.unimplemented
+            return findMD5(messagePrefix: data, matching: hasSixZeroNibblePrefix).toString
         }
 
+        private func findMD5(messagePrefix: Data, matching predicate: ((Data) -> Bool)) -> Int {
+            var digestData = Data(count: Int(CC_MD5_DIGEST_LENGTH))
+            
+            var counter = 0
+            repeat {
+                counter += 1
+                let message = messagePrefix + "\(counter)".data(using: .ascii)!
+                _ = digestData.withUnsafeMutableBytes { digestBytes in
+                    message.withUnsafeBytes { messageBytes in
+                        CC_MD5(messageBytes, CC_LONG(message.count), digestBytes)
+                    }
+                }
+            } while !predicate(digestData)
+            
+            return counter
+        }
+        
         let knownAnswerToExampleForFirstPart = "609043"
         let knownAnswerToFirstPart = "117946"
+        let knownAnswerToSecondPart = "3938038"
     }
 }
 
+@inlinable
+func hasFiveZeroNibblePrefix(_ data: Data) -> Bool {
+    return data.withUnsafeBytes { (ptr: UnsafePointer<UInt64>) -> Bool in
+        return ptr.pointee.bigEndian & 0xFFFF_F000_0000_0000 == 0
+    }
+}
 
+@inlinable
+func hasSixZeroNibblePrefix(_ data: Data) -> Bool {
+    return data.withUnsafeBytes { (ptr: UnsafePointer<UInt64>) -> Bool in
+        return ptr.pointee.bigEndian & 0xFFFF_FF00_0000_0000 == 0
+    }
+}
