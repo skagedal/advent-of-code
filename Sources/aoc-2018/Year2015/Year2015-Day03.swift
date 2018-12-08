f1b26c5b7f8b6ca2ed1d7a89d54fd2c60d1179ee
Simon Kågedal Reimer <skagedal@gmail.com>
2018-12-08 21:22:49 +0100

    2015 day 3 part 1

--- Sources/aoc-2018/Year2015/Year2015-Day03.swift
+++ Sources/aoc-2018/Year2015/Year2015-Day03.swift
@@ -5,11 +5,41 @@ extension Year2015 {
         let day = 3
 
         func answerToFirstPart(_ data: Data) throws -> String {
-            throw AdventError.unimplemented
+            var current: Location = .zero
+            var visited: [Location: Int] = [current: 1]
+            for instruction in data {
+                current = move(instruction, from: current)
+                visited[current, default: 0] += 1
+            }
+            return visited.keys.count.toString
         }
     
         func answerToSecondPart(_ data: Data) throws -> String {
             throw AdventError.unimplemented
         }
+        
+        let knownAnswerToFirstPart = "2592"
+    }
+}
+
+private struct Location: Hashable {
+    let x: Int
+    let y: Int
+
+    static let zero = Location(x: 0, y: 0)
+}
+
+private func move(_ instruction: UInt8, from start: Location) -> Location {
+    switch instruction {
+    case ASCII.circumflexAccent:
+        return Location(x: start.x, y: start.y - 1)
+    case ASCII.v:
+        return Location(x: start.x, y: start.y + 1)
+    case ASCII.lessThanSign:
+        return Location(x: start.x - 1, y: start.y)
+    case ASCII.greaterThanSign:
+        return Location(x: start.x + 1, y: start.y)
+    default:
+        fatalError("Unknown move instruction: \(instruction)")
     }
 }
