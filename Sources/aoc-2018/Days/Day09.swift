673aaf1fc61ee0e1dbbb72d1359f0189a4ce5231
Simon Kågedal Reimer <skagedal@gmail.com>
2018-12-09 09:28:36 +0100

    Add performant solution to 2018 Day 9

--- Sources/aoc-2018/Days/Day09.swift
+++ Sources/aoc-2018/Days/Day09.swift
@@ -5,12 +5,12 @@ struct Day09: AdventDay2018 {
     
     func answerToFirstPart(_ data: Data) throws -> String {
         let input = Array(data.integers)
-        return playGame(players: input[0], lastMarble: input[1]).toString
+        return playFastGame(players: input[0], lastMarble: input[1]).toString
     }
     
     func answerToSecondPart(_ data: Data) throws -> String {
         let input = Array(data.integers)
-        return playGame(players: input[0], lastMarble: input[1] * 100).toString
+        return playFastGame(players: input[0], lastMarble: input[1] * 100).toString
     }
     
     private func playGame(players: Int, lastMarble: Int) -> Int {
@@ -31,6 +31,112 @@ struct Day09: AdventDay2018 {
         return points.max()!
     }
     
+    private func playFastGame(players: Int, lastMarble: Int) -> Int {
+        let board = List<Int>()
+        var currentNode = board.addLast(0)
+        var points = Array(repeating: 0, count: players)
+        var currentPlayer = 0
+        for marble in 1...lastMarble {
+            if marble % 23 == 0 {
+                let removedNode = board.back(7, from: currentNode)
+                currentNode = board.forward(1, from: removedNode)
+                board.remove(removedNode)
+                points[currentPlayer] += marble + removedNode.element
+            } else {
+                currentNode = board.insert(marble, before: board.forward(2, from: currentNode))
+            }
+            currentPlayer = (currentPlayer + 1) % players
+        }
+        return points.max()!
+    }
+    
     let knownAnswerToExampleForFirstPart = "8317"
     let knownAnswerToFirstPart = "437654"
+    let knownAnswerToSecondPart = "3689913905"
+}
+
+class List<Element> {
+    class Node {
+        var element: Element
+        var previous: Node?
+        var next: Node?
+        
+        init(_ element: Element) {
+            self.element = element
+        }
+    }
+    var first: Node?
+    var last: Node?
+    
+    func addLast(_ element: Element) -> Node {
+        let node = Node(element)
+        if let last = last {
+            last.next = node
+            node.previous = last
+            self.last = node
+            return node
+        } else {
+            self.first = node
+            self.last = node
+            return node
+        }
+    }
+    
+    func insert(_ element: Element, before nextNode: Node) -> Node {
+        let node = Node(element)
+        node.previous = nextNode.previous
+        node.next = nextNode
+        nextNode.previous?.next = node
+        nextNode.previous = node
+        
+        if node.previous == nil {
+            first = node
+        }
+        return node
+    }
+    
+    func remove(_ node: Node) {
+        node.previous?.next = node.next
+        node.next?.previous = node.previous
+        if node.next?.previous == nil {
+            first = node.next
+        }
+        if node.previous?.next == nil {
+            last = node.previous
+        }
+    }
+    
+    func back(_ n: Int, from node: Node) -> Node {
+        var current = node
+        for _ in 0..<n {
+            if let previous = current.previous {
+                current = previous
+            } else {
+                current = last!
+            }
+        }
+        return current
+    }
+    
+    func forward(_ n: Int, from node: Node) -> Node {
+        var current = node
+        for _ in 0..<n {
+            if let next = current.next {
+                current = next
+            } else {
+                current = first!
+            }
+        }
+        return current
+    }
+    
+    func printList() {
+        print("[", terminator: "")
+        var node = first
+        while let thisNode = node {
+            print(thisNode.element, terminator: ", ")
+            node = thisNode.next
+        }
+        print("]")
+    }
 }
