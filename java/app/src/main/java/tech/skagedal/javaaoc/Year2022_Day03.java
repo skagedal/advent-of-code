5d188f209cbde41437febe20710113b036a0881d
Simon Kågedal Reimer <skagedal@gmail.com>
2022-12-04 18:12:58 +0100

    Clean upp 2022-3-1

--- java/app/src/main/java/tech/skagedal/javaaoc/Year2022_Day03.java
+++ java/app/src/main/java/tech/skagedal/javaaoc/Year2022_Day03.java
@@ -1,7 +1,12 @@
 package tech.skagedal.javaaoc;
 
+import com.google.common.collect.Sets;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import tech.skagedal.javaaoc.tools.Streams;
@@ -11,9 +16,9 @@ public class Year2022_Day03 extends Year2022Day {
     public long part1() {
         return getLines()
             .map(this::splitInHalf)
-            .map(tuple -> intersection(tuple.value1(), tuple.value2()))
-            .mapToInt(list -> list.get(0))
-            .map(this::getPriority)
+            .map(tuple -> Sets.intersection(toSet(tuple.value1()), toSet(tuple.value2())))
+            .map(theSet -> theSet.iterator().next())
+            .mapToLong(this::getPriority)
             .sum();
     }
 
@@ -28,14 +33,16 @@ public class Year2022_Day03 extends Year2022Day {
         return sum;
     }
 
-    Tuple2<CharSequence, CharSequence> splitInHalf(String line) {
+    Tuple2<String, String> splitInHalf(String line) {
         return new Tuple2<>(
-            line.subSequence(0, line.length() / 2),
-            line.subSequence(line.length() / 2, line.length())
+            line.substring(0, line.length() / 2),
+            line.substring(line.length() / 2, line.length())
         );
     }
 
-
+    Set<Integer> toSet(String string) {
+        return string.chars().boxed().collect(Collectors.toSet());
+    }
 
     List<Integer> intersection(String s1, String s2) {
         return intersection(s1.subSequence(0, s1.length()), s2.subSequence(0, s2.length()));
