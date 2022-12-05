41ecd6d83b6cbfb02b64542881357e99ae2c1ea2
Simon Kågedal Reimer <skagedal@gmail.com>
2022-12-05 21:09:18 +0100

    Simplify structure of 2022-03

--- java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day03.java
+++ java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day03.java
@@ -10,9 +10,9 @@ import tech.skagedal.javaaoc.tools.Tuple2;
 public class Day03 extends Year2022Day {
     public long part1() {
         return readLines()
-            .map(Util::splitStringInHalf)
+            .map(Day03::splitStringInHalf)
             .map(tuple -> Sets.intersection(toSet(tuple.value1()), toSet(tuple.value2())))
-            .map(Util::singleElementOfSet)
+            .map(Day03::singleElementOfSet)
             .mapToLong(this::getPriority)
             .sum();
     }
@@ -20,7 +20,7 @@ public class Day03 extends Year2022Day {
     public long part2() {
         return Streams.splittingFixedSize(readLines().map(this::toSet), 3)
             .map(list -> list.stream().reduce(Sets::intersection).get())
-            .map(Util::singleElementOfSet)
+            .map(Day03::singleElementOfSet)
             .mapToLong(this::getPriority)
             .sum();
     }
@@ -36,9 +36,7 @@ public class Day03 extends Year2022Day {
             return (character - 'A') + 27;
         throw new IllegalArgumentException("Illegal character: " + character);
     }
-}
 
-class Util {
     static Tuple2<String, String> splitStringInHalf(String line) {
         return new Tuple2<>(
             line.substring(0, line.length() / 2),
@@ -55,4 +53,3 @@ class Util {
         return element;
     }
 }
-
