bf5d7a5ca8caa3d12d35b2b5373992a81c00cd07
Simon Kågedal Reimer <skagedal@gmail.com>
2022-12-05 21:26:47 +0100

    2022-05: More cleanup

--- java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day05.java
+++ java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day05.java
@@ -3,30 +3,28 @@ package tech.skagedal.javaaoc.year2022;
 import java.lang.reflect.Array;
 import java.util.Collections;
 import java.util.Stack;
-import java.util.function.Consumer;
 import java.util.regex.Pattern;
 import tech.skagedal.javaaoc.aoc.AocDay;
 import tech.skagedal.javaaoc.tools.Streams;
 
 public class Day05 extends AocDay {
     public String part1() {
-        var cargo = new Cargo();
-        return solvePart(cargo, cargo::addMove);
+        return solvePart(Day05::moveOneByOne);
     }
 
     public String part2() {
-        var cargo = new Cargo();
-        return solvePart(cargo, cargo::addMultiMove);
+        return solvePart(Day05::moveAllTogether);
     }
 
-    private String solvePart(Cargo cargo, Consumer<String> moveProcessor) {
+    private String solvePart(Mover mover) {
         final var lists = Streams.splitting(readLines(), String::isBlank).toList();
-        final var initialCargo = lists.get(0);
+        final var initialCargoDescription = lists.get(0);
         final var moves = lists.get(1);
 
-        Collections.reverse(initialCargo);
-        initialCargo.stream().skip(1).forEach(cargo::addInitialCargoLine);
-        moves.forEach(moveProcessor);
+        final var cargo = new Cargo();
+        Collections.reverse(initialCargoDescription);
+        initialCargoDescription.stream().skip(1).forEach(cargo::addInitialCargoLine);
+        moves.forEach(move -> cargo.perform(move, mover));
         return cargo.topLine();
     }
 
@@ -50,40 +48,50 @@ public class Day05 extends AocDay {
             }
         }
 
-        public void addMove(String line) {
-            final var results = pattern.matcher(line).results().toList();
-
-            final var count = Integer.parseInt(results.get(0).group());
-            final var source =  Integer.parseInt(results.get(1).group()) - 1;
-            final var destination = Integer.parseInt(results.get(2).group()) - 1;
-
-            for (var i = 0; i < count; i++) {
-                stacks[destination].push(stacks[source].pop());
+        public String topLine() {
+            StringBuilder total = new StringBuilder();
+            for (final var stack : stacks) {
+                total.append(stack.peek());
             }
+            return total.toString();
         }
 
-        public String topLine() {
-            var total = "";
-            for (var i = 0; i < stacks.length; i++) {
-                total += stacks[i].peek();
-            }
-            return total;
+        public void perform(String line) {
+            perform(line, Day05::moveOneByOne);
         }
 
         public void addMultiMove(String line) {
+            perform(line, Day05::moveAllTogether);
+        }
+
+        public void perform(String line, Mover mover) {
             final var results = pattern.matcher(line).results().toList();
 
             final var count = Integer.parseInt(results.get(0).group());
             final var source =  Integer.parseInt(results.get(1).group()) - 1;
             final var destination = Integer.parseInt(results.get(2).group()) - 1;
 
-            final var stack = new Stack<String>();
-            for (var i = 0; i < count; i++) {
-                stack.push(stacks[source].pop());
-            }
-            while(!stack.isEmpty()) {
-                stacks[destination].push(stack.pop());
-            }
+            mover.move(count, stacks[source], stacks[destination]);
+        }
+    }
+
+    interface Mover {
+        void move(int count, Stack<String> source, Stack<String> destination);
+    }
+
+    static void moveOneByOne(int count, Stack<String> source, Stack<String> destination) {
+        for (var i = 0; i < count; i++) {
+            destination.push(source.pop());
+        }
+    }
+
+    static void moveAllTogether(int count, Stack<String> source, Stack<String> destination) {
+        final var stack = new Stack<String>();
+        for (var i = 0; i < count; i++) {
+            stack.push(source.pop());
+        }
+        while(!stack.isEmpty()) {
+            destination.push(stack.pop());
         }
     }
 }
