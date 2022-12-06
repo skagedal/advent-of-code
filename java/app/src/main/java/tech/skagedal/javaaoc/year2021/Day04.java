b2767c6b78de83571b2baeafdf8a0b3ca1302eba
Simon Kågedal Reimer <skagedal@gmail.com>
2022-12-06 19:59:16 +0100

    2021-04: Solve part 1

--- java/app/src/main/java/tech/skagedal/javaaoc/year2021/Day04.java
+++ java/app/src/main/java/tech/skagedal/javaaoc/year2021/Day04.java
@@ -1,38 +1,111 @@
 package tech.skagedal.javaaoc.year2021;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 import tech.skagedal.javaaoc.aoc.AocDay;
 import tech.skagedal.javaaoc.tools.Streams;
 
 public class Day04 extends AocDay {
     public long part1() {
-        final var list = Streams.splitting(readLines(), String::isBlank).toList();
-        final var instructions = list.get(0);
+        final var calledNumbers = readLines().findFirst()
+            .map(Day04::parseCalledNumbers)
+            .orElseThrow();
+        final var boards = Streams.splitting(readLines().skip(2), String::isBlank)
+            .map(Board::parse).toList();
 
-
+        for (var number : calledNumbers) {
+            for (var board : boards) {
+                board.mark(number);
+                if (board.isBingo()) {
+                    return board.sumOfUnmarkedNumbers() * number;
+                }
+            }
+        }
+        System.out.printf("top value: %d\n", boards.get(0).getValue(0, 0));
         return 0;
+    }
+
+    private static List<Integer> parseCalledNumbers(String s) {
+        return Arrays.stream(s.split(",")).map(Integer::parseInt).toList();
     }
 
     public long part2() {
         return 0;
     }
 
+    private static Pattern NUMBERS = Pattern.compile("\\d+");
+
     static class Board {
         int[][] number;
-        boolean[][] marked;
+        int[][] marked;
 
-        public Board(int[][] number, boolean[][] marked) {
+        public Board(int[][] number, int[][] marked) {
             this.number = number;
             this.marked = marked;
         }
 
         static Board parse(List<String> lines) {
-            lines.stream()
-                .map(line -> line.)
             return new Board(
-                new int[5][5],
-                new boolean[5][5]
+                lines.stream()
+                    .map(Board::parseRow)
+                    .toArray(int[][]::new),
+                new int[5][5]
             );
+        }
+
+        private static int[] parseRow(String line) {
+            return NUMBERS.matcher(line).results().mapToInt(result -> Integer.parseInt(result.group())).toArray();
+        }
+
+        public int getValue(int row, int column) {
+            return number[row][column];
+        }
+
+        public void mark(int value) {
+            for (var i = 0; i < marked.length; i++) {
+                for (var j = 0; j < marked[i].length; j++) {
+                    if (number[i][j] == value) {
+                        mark(i, j);
+                    }
+                }
+            }
+        }
+
+        public void mark(int row, int column) {
+            marked[row][column] = 1;
+        }
+
+        public boolean isBingo() {
+            return rows(marked).anyMatch(row -> row.allMatch(i -> i == 1))
+                || columns(marked).anyMatch(column -> column.allMatch(i -> i == 1));
+        }
+
+        private static Stream<IntStream> rows(int[][] matrix) {
+            return IntStream.range(0, 5).mapToObj(row -> row(matrix, row));
+        }
+
+        private static Stream<IntStream> columns(int[][] matrix) {
+            return IntStream.range(0, 5).mapToObj(column -> column(matrix, column));
+        }
+
+        private static IntStream row(int[][] matrix, int row) {
+            return Arrays.stream(matrix[row], 0, 5);
+        }
+
+        private static IntStream column(int[][] matrix, int column) {
+            return IntStream.range(0, 5).map(row -> matrix[row][column]);
+        }
+
+        public long sumOfUnmarkedNumbers() {
+            return Streams.zip(rows(number), rows(marked))
+                .flatMap(tuple -> Streams.zip(tuple.value1().boxed(), tuple.value2().boxed()).
+                    map(tuple2 -> tuple2.value1() * (1 - tuple2.value2())))
+                .mapToInt(Integer::intValue)
+                .sum();
         }
     }
 }
--- java/app/src/test/java/tech/skagedal/javaaoc/year2021/Day04Test.java
+++ java/app/src/test/java/tech/skagedal/javaaoc/year2021/Day04Test.java
@@ -1,6 +1,8 @@
 package tech.skagedal.javaaoc.year2021;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +11,7 @@ class Day04Test {
     void part1() {
         final var answer = new Day04().part1();
 
-        assertEquals(-1, answer);
+        assertEquals(63424, answer);
     }
 
     @Test
@@ -17,5 +19,80 @@ class Day04Test {
         final var answer = new Day04().part2();
 
         assertEquals(-1, answer);
+    }
+
+    @Test
+    void parseBoard() {
+        var lines = """
+            22 13 17 11  0
+             8  2 23  4 24
+            21  9 14 16  7
+             6 10  3 18  5
+             1 12 20 15 19""".lines().toList();
+        var board = Day04.Board.parse(lines);
+
+        assertEquals(22, board.getValue(0, 0));
+        assertEquals(14, board.getValue(2, 2));
+        assertEquals(6, board.getValue(3, 0));
+    }
+
+    @Test
+    void noBingo() {
+        var board = new Day04.Board(new int[5][5], new int[][] {
+            {0, 0, 0, 0, 0},
+            {1, 1, 0, 1, 1},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0}
+        });
+
+        assertFalse(board.isBingo());
+    }
+
+    @Test
+    void bingoRow() {
+        var board = new Day04.Board(new int[5][5], new int[][] {
+            {0, 0, 0, 0, 0},
+            {1, 1, 1, 1, 1},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0}
+        });
+
+        assertTrue(board.isBingo());
+    }
+
+    @Test
+    void bingoColumn() {
+        var board = new Day04.Board(new int[5][5], new int[][] {
+            {0, 0, 0, 0, 1},
+            {1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1}
+        });
+
+        assertTrue(board.isBingo());
+    }
+
+    @Test
+    void sumOfUnmarkedNumbers() {
+        var board = new Day04.Board(
+            new int[][] {
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 0},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5}
+            },
+            new int[][] {
+                {0, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1}
+            });
+
+        assertEquals(10, board.sumOfUnmarkedNumbers());
     }
 }
