7a557d21834cd4ef509365b78608eec8e0022e86
Simon Kågedal Reimer <skagedal@gmail.com>
2022-12-06 12:06:01 +0100

    Add a way to stream chars instead of lines from file

--- java/app/src/main/java/tech/skagedal/javaaoc/aoc/AocDay.java
+++ java/app/src/main/java/tech/skagedal/javaaoc/aoc/AocDay.java
@@ -1,10 +1,13 @@
 package tech.skagedal.javaaoc.aoc;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.function.IntSupplier;
 import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class AocDay {
@@ -28,20 +31,65 @@ public class AocDay {
     }
 
     protected Stream<String> readLines() {
-        final var match = fileNameFromFQCN.matcher(this.getClass().getName())
-            .results().findFirst().orElseThrow();
-        return readLines(
-            match.group(1).toLowerCase(),
-            match.group(2).toLowerCase() + "_input.txt"
-        );
+        return readLines(getPath());
     }
 
-    private Stream<String> readLines(String yearDirectory, String filename) {
-        final var path = findData().resolve(yearDirectory).resolve(filename);
+    protected IntStream readChars() {
+        return readChars(getPath());
+    }
+
+    private Path getPath() {
+        final var match = fileNameFromFQCN.matcher(this.getClass().getName())
+            .results().findFirst().orElseThrow();
+        final var path = findData().resolve(match.group(1).toLowerCase()).resolve(
+            match.group(2).toLowerCase() + "_input.txt");
+        return path;
+    }
+
+    private static Stream<String> readLines(Path path) {
         try {
             return Files.newBufferedReader(path).lines();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private IntStream readChars(Path path) {
+        try {
+            BufferedReader reader = Files.newBufferedReader(path);
+            return IntStream
+                .generate(checkSupplier(reader::read))
+                .onClose(checkRunnable(reader::close));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    interface SneakyIntSupplier {
+        int supply() throws Exception;
+    }
+
+    interface SneakyRunnable {
+        void run() throws Exception;
+    }
+
+    private IntSupplier checkSupplier(SneakyIntSupplier supplier) {
+        return () -> {
+            try {
+                return supplier.supply();
+            } catch (Exception exception) {
+                throw new RuntimeException(exception);
+            }
+        };
+    }
+
+    private Runnable checkRunnable(SneakyRunnable runnable) {
+        return () -> {
+            try {
+                runnable.run();;
+            } catch (Exception exception) {
+                throw new RuntimeException(exception);
+            }
+        };
     }
 }
--- java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day06.java
+++ java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day06.java
@@ -8,17 +8,16 @@ import tech.skagedal.javaaoc.tools.Strings;
 
 public class Day06 extends AocDay {
     public long part1() {
-        return solveImperative(4);
+        return solveFunctional(4);
     }
 
     public long part2() {
-        return solveImperative(14);
+        return solveFunctional(14);
     }
 
     private int solveFunctional(int packetSize) {
-        final var line = readLines().findFirst().orElseThrow();
         return Streams.zip(
-                Streams.splittingFixedSizeOverlapping(line.chars().boxed(), packetSize, packetSize - 1),
+                Streams.splittingFixedSizeOverlapping(readChars().boxed(), packetSize, packetSize - 1),
                 IntStream.iterate(0, i -> i + 1).boxed())
             .filter(tuple -> Sets.newHashSet(tuple.value1()).size() == packetSize)
             .findFirst().orElseThrow()
