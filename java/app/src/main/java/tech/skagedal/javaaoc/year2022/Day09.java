b6297e619e11eda25e9e8017293e7da4ae160d18
Simon Kågedal Reimer <skagedal@gmail.com>
2022-12-09 06:33:32 +0100

    2022-09: Skeleton

---
+++ java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day09.java
@@ -0,0 +1,19 @@
+package tech.skagedal.javaaoc.year2022;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+import tech.skagedal.javaaoc.aoc.AdventOfCode;
+import tech.skagedal.javaaoc.aoc.AocDay;
+import tech.skagedal.javaaoc.tools.Streams;
+
+@AdventOfCode
+public class Day09 extends AocDay {
+    public long part1() {
+        return 0;
+    }
+
+    public long part2() {
+        return 0;
+    }
+}
---
+++ java/app/src/test/java/tech/skagedal/javaaoc/year2022/Day09Test.java
@@ -0,0 +1,24 @@
+package tech.skagedal.javaaoc.year2022;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class Day09Test {
+    private final Day09 day = new Day09();
+
+    @Test
+    void part1() {
+        final var answer = day.part1();
+
+        assertEquals(-1, answer);;
+    }
+
+    @Test
+    void part2() {
+        final var answer = day.part2();
+
+        assertEquals(-1, answer);
+    }
+
+}
