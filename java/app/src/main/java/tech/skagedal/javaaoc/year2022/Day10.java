df5dd8bd5bae3f7bc03ce3d6f62d57ddcfdb968a
Simon Kågedal Reimer <skagedal@gmail.com>
2022-12-10 09:06:04 +0100

    2022-10: Add real data, refactor part 1 somewhat

--- java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day10.java
+++ java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day10.java
@@ -1,37 +1,47 @@
 package tech.skagedal.javaaoc.year2022;
 
+import java.util.stream.Stream;
 import tech.skagedal.javaaoc.aoc.AocDay;
 import tech.skagedal.javaaoc.tools.Streams;
 
 public class Day10 extends AocDay {
-    private int signalStrengthSum;
 
     public long part1() {
-        var currentCycle = 1;
-        var currentX = 1;
-        for (var instruction : Streams.toIterable(readExampleLines().map(Day10::parse))) {
-            switch (instruction) {
-                case Instruction.Noop noop -> {
-                    emit(currentCycle++, currentX);
-                }
-                case Instruction.Addx addx -> {
-                    emit(currentCycle++, currentX);
-                    emit(currentCycle++, currentX);
-                    currentX += addx.value();
+        final var device = new Device();
+        device.process(readLines().map(Day10::parse));
+        return device.signalStrengthSum;
+    }
+
+    private static class Device {
+        int signalStrengthSum = 0;
+        int currentCycle = 1;
+        int currentX = 1;
+
+        void process(Stream<Instruction> instructions) {
+            for (var instruction : Streams.toIterable(instructions)) {
+                switch (instruction) {
+                    case Instruction.Noop noop -> {
+                        emit(currentCycle++, currentX);
+                    }
+                    case Instruction.Addx addx -> {
+                        emit(currentCycle++, currentX);
+                        emit(currentCycle++, currentX);
+                        currentX += addx.value();
+                    }
                 }
             }
         }
-        return signalStrengthSum;
-    }
 
-    private void emit(int cycle, int x) {
-        int signalStrength = cycle * x;
-        System.out.printf("[%d] x is %d - cycle strength is %d\n", cycle, x, signalStrength);
-        if ((cycle - 20) % 40 == 0) {
-            System.out.printf("SAMPLING %d\n", signalStrength);
-            signalStrengthSum += signalStrength;
+        private void emit(int cycle, int x) {
+            int signalStrength = cycle * x;
+            System.out.printf("[%d] x is %d - cycle strength is %d\n", cycle, x, signalStrength);
+            if ((cycle - 20) % 40 == 0) {
+                System.out.printf("SAMPLING %d\n", signalStrength);
+                signalStrengthSum += signalStrength;
+            }
         }
     }
+
 
     private static Instruction parse(String s) {
         if (s.equals("noop")) {
