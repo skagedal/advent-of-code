5b826e10d9cc860fbc2a997fc0d05ae55b51cc5e
Simon Kågedal Reimer <skagedal@gmail.com>
2022-12-12 21:26:22 +0100

    2022-12: Solve part 1

--- java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day12.java
+++ java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day12.java
@@ -1,10 +1,13 @@
 package tech.skagedal.javaaoc.year2022;
 
 import com.google.common.primitives.Longs;
+import java.util.HashSet;
 import java.util.OptionalLong;
+import java.util.Set;
 import java.util.Stack;
 import java.util.function.IntFunction;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import tech.skagedal.javaaoc.aoc.AocDay;
 import tech.skagedal.javaaoc.tools.format.Term;
@@ -13,79 +16,62 @@ import tech.skagedal.javaaoc.tools.linear.Point;
 
 public class Day12 extends AocDay {
     public long part1() {
-        return solvePart1(readExampleLines());
+        return solveProperly(readLines());
     }
 
-    public long solvePart1(Stream<String> lines) {
-        return solvePart1(lines, Square.LOWEST_HEIGHT);
+    public long solveProperly(Stream<String> lines) {
+        // Read the grid.
+        final var grid = Grid.fromLines(lines, Square::from);
+        // We need to do graph sorting.
+        final var remainingNodes = grid.allPoints().collect(Collectors.toCollection(HashSet::new));
+        // First find the nodes that are have distance 0.
+        final var endNodes = grid.allPoints()
+            .filter(point -> grid.get(point).distance instanceof Distance.Known known && known.steps() == 0)
+            .collect(Collectors.toSet());
+        final Stack<Set<Point>> stack = new Stack<>();
+
+        stack.push(endNodes);
+        remainingNodes.removeAll(endNodes);
+
+        // When we find the set that contains our Start node, then that's the distance.
+        return solveProperly(grid, remainingNodes, stack);
     }
 
-    public long solvePart1(Stream<String> lines, int startHeight) {
-        var grid = Grid.fromLines(lines, Square.fromIntWithStartHeight(startHeight));
-        var start = grid
-            .allPoints()
-            .filter(point -> grid.get(point).distance instanceof Distance.Start)
-            .findFirst().orElseThrow();
+    private long solveProperly(Grid<Square> grid, HashSet<Point> remaining, Stack<Set<Point>> stack) {
+        // Then for each iteration, find the nodes that lead into the previous set.
+        // Remove them from the full set.
 
-        var stack = new Stack<Point>();
-        long returnval = explore(start, grid, "", stack).orElseThrow();
-        return returnval;
+        final var last = stack.peek();
+        final var previous = (stack.size() > 1) ? stack.elementAt(stack.size() - 2) : Set.of();
+//        System.out.printf("== %d ==\n", stack.size());
+//        grid.printGrid(point -> {
+//            final var height = grid.get(point).height;
+//            return
+//                (last.contains(point) ? Term.BG_GREEN :
+//                    (previous.contains(point) ? Term.BG_YELLOW : "")) +
+//                String.format("%c", height) +
+//                Term.RESET;
+//        });
+//        System.out.println();
 
-    }
-
-    private OptionalLong explore(Point point, Grid<Square> grid, String indent, Stack<Point> stack) {
-        // This isn't working.
-
-        stack.push(point);
-        System.out.printf(indent + "Exploring point %s...\n", point);
-        final var square = grid.get(point);
-
-        grid.printGrid(p -> {
-            final var s = grid.get(p);
-            final var startAnsi = p.equals(point) ? Term.FG_GREEN : (stack.contains(p) ? Term.FG_YELLOW : "");
-            final var stopAnsi = Term.RESET;
-            return startAnsi + switch (s.distance) {
-                case Distance.Known known -> String.format("%02d", known.steps());
-                case Distance.Unknown unknown -> "--";
-                case Distance.Unreachable unreachable -> "!!";
-                case Distance.Start start -> "SS";
-            } + stopAnsi + " ";
-        });
-
-        long[] longs = Grid.fourDirections()
-            .map(point::plus)
-            .filter(grid::isInBounds)
-            .filter(Predicate.not(stack::contains))
-            .flatMapToLong(point2 -> consider(grid, square, point, point2, indent + " ", stack).stream())
-            .toArray();
-
-        if (longs.length > 0) {
-            Distance.Known knownDistance = new Distance.Known(Longs.min(longs) + 1);
-            square.distance = knownDistance;
-            stack.pop();
-            return OptionalLong.of(knownDistance.steps());
-        } else {
-            square.distance = Distance.UNREACHABLE;
-            stack.pop();
-            return OptionalLong.empty();
+        // Find all the nodes that lead into the points into `last`.
+        final var newSet = remaining.stream()
+            .filter(p1 -> last.stream().anyMatch(p2 -> hasEdgeTo(grid, p1, p2)))
+            .collect(Collectors.toSet());
+        System.out.printf("New nodes: %s\n", newSet);
+        // If those nodes include the start node, return `taken.size()`
+        if (newSet.stream().anyMatch(p -> grid.get(p).distance instanceof Distance.Start)) {
+            return stack.size();
         }
+        // else, recurse
+        remaining.removeAll(newSet);
+        stack.push(newSet);
+        return solveProperly(grid, remaining, stack);
     }
 
-    private OptionalLong consider(Grid<Square> grid, Square square, Point point, Point nextPoint, String indent,
-                                  Stack<Point> stack) {
-        final var nextSquare = grid.get(nextPoint);
-        System.out.printf(indent + "Testing point %s -> %s... ", point, nextPoint);
-        if (nextSquare.height > square.height + 1) {
-            System.out.printf("too high; can't reach %c from %c\n", nextSquare.height, square.height);
-            return OptionalLong.empty();
-        }
-        System.out.printf("yes, can reach %c from %c\n", nextSquare.height, square.height);
-        return switch (nextSquare.distance) {
-            case Distance.Known known -> OptionalLong.of(known.steps());
-            case Distance.Unknown unknown -> explore(nextPoint, grid, indent + " ", stack);
-            case Distance.Unreachable unreachable -> OptionalLong.empty();
-            case Distance.Start start -> OptionalLong.empty();
-        };
+    private boolean hasEdgeTo(Grid<Square> grid, Point p1, Point p2) {
+        return (Math.abs(p1.x() - p2.x()) + Math.abs(p1.y() - p2.y()) == 1)
+         && grid.get(p2).height <= grid.get(p1).height + 1;
     }
 
     private static class Square {
