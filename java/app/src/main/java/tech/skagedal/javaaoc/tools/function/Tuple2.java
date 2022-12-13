4ce963af2bcf33f223a077ff7f638792c61a20b6
Simon Kågedal Reimer <skagedal@gmail.com>
2022-12-13 16:32:32 +0100

    Add splat tool

--- java/app/src/main/java/tech/skagedal/javaaoc/tools/function/Tuple2.java
+++ java/app/src/main/java/tech/skagedal/javaaoc/tools/function/Tuple2.java
@@ -1,4 +1,11 @@
 package tech.skagedal.javaaoc.tools.function;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 public record Tuple2<T1, T2>(T1 value1, T2 value2) {
+    // Not fully sure this is what's called "splat" but it can be useful anyway
+    public static <T1, T2, R> Function<Tuple2<T1, T2>, R> splat(BiFunction<T1, T2, R> biFunction) {
+        return tuple -> biFunction.apply(tuple.value1(), tuple.value2());
+    }
 }
