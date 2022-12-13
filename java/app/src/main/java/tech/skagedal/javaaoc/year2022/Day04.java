c6455f542544fb77034b042ccac61231ce6eeeff
Simon Kågedal Reimer <skagedal@gmail.com>
2022-12-13 16:38:34 +0100

    2022-04: Clean up

--- java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day04.java
+++ java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day04.java
@@ -3,27 +3,37 @@ package tech.skagedal.javaaoc.year2022;
 import com.google.common.collect.Range;
 import tech.skagedal.javaaoc.aoc.AdventOfCode;
 import tech.skagedal.javaaoc.aoc.AocDay;
-import tech.skagedal.javaaoc.tools.function.Tuple2;
 
 @AdventOfCode
 public class Day04 extends AocDay {
     public long part1() {
         return readLines()
-            .map(Day04::parseTuplesOfRanges)
-            .filter(pair -> pair.value1().encloses(pair.value2()) || pair.value2().encloses(pair.value1()))
+            .map(RangePair::fromString)
+            .filter(RangePair::oneEnclosesTheOther)
             .count();
     }
 
     public long part2() {
         return readLines()
-            .map(Day04::parseTuplesOfRanges)
-            .filter(pair -> pair.value1().isConnected(pair.value2()))
+            .map(RangePair::fromString)
+            .filter(RangePair::connected)
             .count();
     }
 
-    private static Tuple2<Range<Integer>, Range<Integer>> parseTuplesOfRanges(String s) {
-        final var split = s.split(",");
-        return new Tuple2<>(parseRange(split[0]), parseRange(split[1]));
+    private record RangePair(Range<Integer> range1, Range<Integer> range2) {
+        static RangePair fromString(String s) {
+            final var split = s.split(",");
+            return new RangePair(parseRange(split[0]), parseRange(split[1]));
+        }
+
+        boolean oneEnclosesTheOther() {
+            return range1.encloses(range2) || range2.encloses(range1);
+        }
+
+        boolean connected() {
+            return range1.isConnected(range2);
+        }
+
     }
 
     private static Range<Integer> parseRange(String s) {
