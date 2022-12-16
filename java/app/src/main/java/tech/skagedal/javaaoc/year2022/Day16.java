f8ff0f24c9d249d7d469b93821ec25d4157c64a8
Simon Kågedal Reimer <skagedal@gmail.com>
2022-12-16 10:13:35 +0100

    Refactor, add part2 skeleton

--- java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day16.java
+++ java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day16.java
@@ -12,7 +12,6 @@ import java.util.stream.Collectors;
 import tech.skagedal.javaaoc.aoc.AdventContext;
 import tech.skagedal.javaaoc.aoc.AdventOfCode;
 import tech.skagedal.javaaoc.aoc.AdventOfCodeRunner;
-import tech.skagedal.javaaoc.tools.function.Tuple2;
 import tech.skagedal.javaaoc.tools.math.Longs;
 import tech.skagedal.javaaoc.tools.string.Strings;
 
@@ -20,19 +19,16 @@ import tech.skagedal.javaaoc.tools.string.Strings;
     description = "Proboscidea Volcanium"
 )
 public class Day16 {
-    Map<String, Long> cache = new HashMap<>();
-    boolean explain = false;
-
     public long part1(AdventContext context) {
-        cache.clear();
-        explain = context.explain();
         final var valves = readValves(context);
-        return maxPressure(valves.get("AA"), valves, 30, Set.of());
+        final var finder = new PressureFinder(valves, context.explain());
+        return finder.maxPressure(valves.get("AA"), 30, Set.of());
     }
 
-    public long part2disabled(AdventContext context) {
-        cache.clear();
-        return 0;
+    public long part2(AdventContext context) {
+        final var valves = readValves(context);
+        final var finder = new PressureFinder(valves, context.explain());
+        return finder.maxPressureWithElephant(valves.get("AA"), valves.get("AA"), 26, Set.of());
     }
 
     private static Map<String, Valve> readValves(AdventContext context) {
@@ -42,42 +38,91 @@ public class Day16 {
         ));
     }
 
-    long maxPressure(Valve valve, Map<String, Valve> valves, int minutes, Set<String> openValves) {
-        if (explain) System.out.printf("== Considering valve %s at %d minutes left ==\n", valve.id, minutes);
-        if (minutes == 0) {
-            return 0;
+    static class PressureFinder {
+        private final Map<String, Valve> valves;
+        private final boolean explain;
+        private Map<String, Long> cache = new HashMap<>();
+
+        PressureFinder(Map<String, Valve> valves, boolean explain) {
+            this.valves = valves;
+            this.explain = explain;
         }
 
-        final var cacheKey = String.format("%s-%d-%s", valve.id, minutes, openValves.stream().sorted().collect(Collectors.joining()));
-        final var cachedValue = cache.get(cacheKey);
-        if (cachedValue != null) {
-            return cachedValue;
+        long maxPressure(Valve valve, int minutes, Set<String> openValves) {
+            if (explain) System.out.printf("== Considering valve %s at %d minutes left ==\n", valve.id, minutes);
+            if (minutes == 0) {
+                return 0;
+            }
+
+            final var cacheKey = String.format("%s-%d-%s", valve.id, minutes, openValves.stream().sorted().collect(Collectors.joining()));
+            final var cachedValue = cache.get(cacheKey);
+            if (cachedValue != null) {
+                return cachedValue;
+            }
+
+            final var id = String.format("%s:%02d", valve.id, minutes);
+            Map<String, Long> scoreOptions = new HashMap<>();
+
+            if (valve.flowRate == 0) {
+                if (explain) System.out.printf("[%s] No point in opening, no flow\n", id);
+            } else if (openValves.contains(valve.id)) {
+                if (explain) System.out.printf("[%s] Already open\n", id);
+            } else {
+                if (explain) System.out.printf("[%s] We could open this vault. It takes one minute to open.\n", id);
+                long scoreForOpening = (minutes - 1) * valve.flowRate;
+                if (explain) System.out.printf("[%s] So the score for just this would be %d\n", id, scoreForOpening);
+                long scoreForRest = maxPressure(valve, minutes - 1, Sets.union(openValves, Set.of(valve.id)));
+                scoreOptions.put(valve.id, scoreForOpening + scoreForRest);
+            }
+
+            for (var otherValve : valve.tunnels) {
+                final var scoreForMoving = maxPressure(valves.get(otherValve), minutes - 1, openValves);
+                scoreOptions.put(otherValve, scoreForMoving);
+            }
+
+            final var maxOption = scoreOptions.entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).orElseThrow();
+            if (explain) System.out.printf("[%s] We chose option %s, worth %d\n", id, maxOption.getKey(), maxOption.getValue());
+            cache.put(cacheKey, maxOption.getValue());
+            return maxOption.getValue();
         }
 
-        final var id = String.format("%s:%02d", valve.id, minutes);
-        Map<String, Long> scoreOptions = new HashMap<>();
+        long maxPressureWithElephant(Valve valve, Valve elephantsValve, int minutes, Set<String> openValves) {
+            if (explain) System.out.printf("== Considering valve %s at %d minutes left ==\n", valve.id, minutes);
+            if (minutes == 0) {
+                return 0;
+            }
 
-        if (valve.flowRate == 0) {
-            if (explain) System.out.printf("[%s] No point in opening, no flow\n", id);
-        } else if (openValves.contains(valve.id)) {
-            if (explain) System.out.printf("[%s] Already open\n", id);
-        } else {
-            if (explain) System.out.printf("[%s] We could open this vault. It takes one minute to open.\n", id);
-            long scoreForOpening = (minutes - 1) * valve.flowRate;
-            if (explain) System.out.printf("[%s] So the score for just this would be %d\n", id, scoreForOpening);
-            long scoreForRest = maxPressure(valve, valves, minutes - 1, Sets.union(openValves, Set.of(valve.id)));
-            scoreOptions.put(valve.id, scoreForOpening + scoreForRest);
+            final var cacheKey = String.format("%s-%d-%s", valve.id, minutes, openValves.stream().sorted().collect(Collectors.joining()));
+            final var cachedValue = cache.get(cacheKey);
+            if (cachedValue != null) {
+                return cachedValue;
+            }
+
+            final var id = String.format("%s:%02d", valve.id, minutes);
+            Map<String, Long> scoreOptions = new HashMap<>();
+
+            if (valve.flowRate == 0) {
+                if (explain) System.out.printf("[%s] No point in opening, no flow\n", id);
+            } else if (openValves.contains(valve.id)) {
+                if (explain) System.out.printf("[%s] Already open\n", id);
+            } else {
+                if (explain) System.out.printf("[%s] We could open this vault. It takes one minute to open.\n", id);
+                long scoreForOpening = (minutes - 1) * valve.flowRate;
+                if (explain) System.out.printf("[%s] So the score for just this would be %d\n", id, scoreForOpening);
+                long scoreForRest = maxPressure(valve, minutes - 1, Sets.union(openValves, Set.of(valve.id)));
+                scoreOptions.put(valve.id, scoreForOpening + scoreForRest);
+            }
+
+            for (var otherValve : valve.tunnels) {
+                final var scoreForMoving = maxPressure(valves.get(otherValve), minutes - 1, openValves);
+                scoreOptions.put(otherValve, scoreForMoving);
+            }
+
+            final var maxOption = scoreOptions.entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).orElseThrow();
+            if (explain) System.out.printf("[%s] We chose option %s, worth %d\n", id, maxOption.getKey(), maxOption.getValue());
+            cache.put(cacheKey, maxOption.getValue());
+            return maxOption.getValue();
         }
-
-        for (var otherValve : valve.tunnels) {
-            final var scoreForMoving = maxPressure(valves.get(otherValve), valves, minutes - 1, openValves);
-            scoreOptions.put(otherValve, scoreForMoving);
-        }
-
-        final var maxOption = scoreOptions.entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).orElseThrow();
-        if (explain) System.out.printf("[%s] We chose option %s, worth %d\n", id, maxOption.getKey(), maxOption.getValue());
-        cache.put(cacheKey, maxOption.getValue());
-        return maxOption.getValue();
     }
 
     record Valve(String id, long flowRate, List<String> tunnels) {
@@ -90,17 +135,17 @@ public class Day16 {
     }
 
     public static void main(String[] args) {
-//        AdventOfCodeRunner.example(new Day16(), """
-//            Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-//            Valve BB has flow rate=13; tunnels lead to valves CC, AA
-//            Valve CC has flow rate=2; tunnels lead to valves DD, BB
-//            Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
-//            Valve EE has flow rate=3; tunnels lead to valves FF, DD
-//            Valve FF has flow rate=0; tunnels lead to valves EE, GG
-//            Valve GG has flow rate=0; tunnels lead to valves FF, HH
-//            Valve HH has flow rate=22; tunnel leads to valve GG
-//            Valve II has flow rate=0; tunnels lead to valves AA, JJ
-//            Valve JJ has flow rate=21; tunnel leads to valve II""");
-        AdventOfCodeRunner.run(new Day16());
+        AdventOfCodeRunner.example(new Day16(), """
+            Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+            Valve BB has flow rate=13; tunnels lead to valves CC, AA
+            Valve CC has flow rate=2; tunnels lead to valves DD, BB
+            Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+            Valve EE has flow rate=3; tunnels lead to valves FF, DD
+            Valve FF has flow rate=0; tunnels lead to valves EE, GG
+            Valve GG has flow rate=0; tunnels lead to valves FF, HH
+            Valve HH has flow rate=22; tunnel leads to valve GG
+            Valve II has flow rate=0; tunnels lead to valves AA, JJ
+            Valve JJ has flow rate=21; tunnel leads to valve II""");
+//        AdventOfCodeRunner.run(new Day16());
     }
 }
