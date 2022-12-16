02474c587b63b4463574660e0e7f513e7e58f27b
Simon Kågedal Reimer <skagedal@gmail.com>
2022-12-16 09:44:04 +0100

    Make download script download up to today

--- download-data/download.sh
+++ download-data/download.sh
@@ -2,7 +2,7 @@
 
 data_dir="${HOME}/.aoc/data"
 
-for year in $(seq 2015 2021); do
+for year in $(seq 2015 2022); do
     echo "== $year =="
     year_dir="$data_dir/year$year"
     mkdir -p "$year_dir"
@@ -10,6 +10,8 @@ for year in $(seq 2015 2021); do
         day_file=$(printf "$year_dir/day%02d_input.txt" $day)
         if test -f "$day_file"; then
             echo "$day_file already exists - skipping"
+        elif [ "$(gdate --date="$(printf "%s-12-%02d EST" $year $day)" +%s)" -gt "$(gdate +%s)" ]; then
+            echo "Not downloading $day_file - time has not come"
         else
             curl -s -S -f "https://adventofcode.com/$year/day/$day/input" -H "$COOKIE" -o $day_file
             echo "Downloaded $day_file"
