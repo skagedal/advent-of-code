d34a49843f024407c1b78eabc62283201bed910f
Simon Kågedal Reimer <skagedal@gmail.com>
2022-12-17 19:40:09 +0100

    2022-17: Solve part 1

---
+++ java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day17.java
@@ -0,0 +1,161 @@
+package tech.skagedal.javaaoc.year2022;
+
+import java.util.Arrays;
+import tech.skagedal.javaaoc.aoc.AdventContext;
+import tech.skagedal.javaaoc.aoc.AdventOfCode;
+import tech.skagedal.javaaoc.aoc.AdventOfCodeRunner;
+
+@AdventOfCode
+public class Day17 {
+    public long part1(AdventContext context) {
+        return new Game(context.line(), context.explain(), 2022).run();
+    }
+
+    public long part2x(AdventContext context) {
+        return new Game(context.line(), context.explain(), 1000000000000L).run();
+    }
+
+
+    private static final char[][] shapes = {
+        {
+            0b0011110
+        },
+        {
+            0b0001000,
+            0b0011100,
+            0b0001000
+        },
+        {
+            0b0000100,
+            0b0000100,
+            0b0011100,
+        },
+        {
+            0b0010000,
+            0b0010000,
+            0b0010000,
+            0b0010000
+        },
+        {
+            0b0011000,
+            0b0011000
+        }
+    };
+
+    static class Game {
+        private final String instructions;
+        private final boolean explain;
+        private final long rounds;
+        private int currentShape = 0;
+        private int currentInstruction = 0;
+        private char[] buf = new char[1024];
+        private int topOfBuf = 0;
+
+        Game(String instructions, boolean explain, long rounds) {
+            this.instructions = instructions;
+            this.explain = explain;
+            this.rounds = rounds;
+        }
+
+        long run() {
+            for (long i = 0; i < rounds; i++) {
+                placeRock();
+                if (explain) {
+                    System.out.printf("== %d ==\n", i);
+                    printRocks();
+                }
+            }
+            return topOfBuf;
+        }
+
+        private void printRocks() {
+            for (int i = topOfBuf - 1; i >= 0; i--) {
+                System.out.println(
+                    String.format("%7s", Integer.toBinaryString(buf[i]))
+                        .replaceAll("[0\\s]", "·")
+                        .replaceAll("1", "■"));
+            }
+        }
+
+        private void placeRock() {
+            char[] rock = Arrays.copyOf(shapes[currentShape], shapes[currentShape].length);
+            currentShape = (currentShape + 1) % shapes.length;
+
+            var currentPosition = topOfBuf + 3;
+            ensureSize(currentPosition + rock.length);
+            while(true) {
+                char instruction = instructions.charAt(currentInstruction);
+                currentInstruction = (currentInstruction + 1) % instructions.length();
+
+                attemptInstruction(instruction, rock, currentPosition);
+                if (isPositionValid(rock, currentPosition - 1)) {
+                    currentPosition--;
+                } else {
+                    break;
+                }
+            };
+            stopRock(rock, currentPosition);
+        }
+
+        private boolean isPositionValid(char[] rock, int position) {
+            if (position < 0) {
+                return false;
+            }
+            for (var i = 0; i < rock.length; i++) {
+                if ((buf[position + i] & rock[rock.length - i - 1]) != 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void attemptInstruction(char instruction, char[] rock, int position) {
+            switch (instruction) {
+                case '<' -> {
+                    for (var i = 0; i < rock.length; i++) {
+                        // Flows over right edge?
+                        if ((rock[rock.length - i - 1] & 0b1000000) != 0) return;
+                        // Clashes with stopped rock?
+                        if ((buf[position + i] & rock[rock.length - i - 1] << 1) != 0) return;
+                    }
+                    for (var i = 0; i < rock.length; i++) {
+                        rock[i] <<= 1;
+                    }
+                }
+                case '>' -> {
+                    for (var i = 0; i < rock.length; i++) {
+                        // Flows over right edge?
+                        if ((rock[rock.length - i - 1] & 0b0000001) != 0) return;
+                        // Clashes with stopped rock?
+                        if ((buf[position + i] & rock[rock.length - i - 1] >> 1) != 0) return;
+                    }
+                    for (var i = 0; i < rock.length; i++) {
+                        rock[i] >>= 1;
+                    }
+                }
+                default -> throw new IllegalStateException("Unknown instruction: " + instruction);
+            }
+        }
+
+        private void stopRock(char[] rock, int start) {
+            topOfBuf = Integer.max(topOfBuf, start + rock.length);
+            for (int i = 0; i < rock.length; i++) {
+                buf[start + i] |= rock[rock.length - i - 1];
+            }
+        }
+
+        private void ensureSize(long size) {
+            if (size >= buf.length) {
+                buf = Arrays.copyOf(buf, buf.length * 2);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+//        AdventOfCodeRunner.example(
+//            new Day17(),
+//            ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
+//        );
+        AdventOfCodeRunner.run(new Day17());
+    }
+}
