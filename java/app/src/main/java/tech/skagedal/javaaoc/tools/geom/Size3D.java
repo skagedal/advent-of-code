accb16f9670aca24599dd36dc48d51f26b080944
Simon Kågedal Reimer <skagedal@gmail.com>
2022-12-20 18:00:53 +0100

    2015-02: Solve part 2

--- java/app/src/main/java/tech/skagedal/javaaoc/tools/geom/Size3D.java
+++ java/app/src/main/java/tech/skagedal/javaaoc/tools/geom/Size3D.java
@@ -4,4 +4,8 @@ public record Size3D(int width, int height, int depth) {
     public Vector3D toVector() {
         return new Vector3D(width, height, depth);
     }
+
+    public int volume() {
+        return width * height * depth;
+    }
 }
--- java/app/src/main/java/tech/skagedal/javaaoc/year2015/Day02.java
+++ java/app/src/main/java/tech/skagedal/javaaoc/year2015/Day02.java
@@ -16,6 +16,13 @@ public class Day02 {
             .sum();
     }
 
+    public long part2(AdventContext context) {
+        return context.lines()
+            .map(line -> Ints.inString(line, Size3D::new))
+            .mapToInt(this::ribbonNeeded)
+            .sum();
+    }
+
     private int wrappingNeeded(Size3D size) {
         final var sides = List.of(
             size.width() * size.height(),
@@ -24,6 +31,15 @@ public class Day02 {
         );
         return sides.stream().mapToInt(side -> side * 2).sum() +
             sides.stream().mapToInt(Integer::intValue).min().orElseThrow();
+    }
+
+    private int ribbonNeeded(Size3D size) {
+        final var perimeters = List.of(
+            size.width() * 2 + size.height() * 2,
+            size.width() * 2 + size.depth() * 2,
+            size.height() * 2 + size.depth() * 2
+        );
+        return perimeters.stream().mapToInt(Integer::intValue).min().orElseThrow() + size.volume();
     }
 
 }
