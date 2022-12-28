8100d7582a2daf477c43b9a226fec6a7ca3eee1f
Simon Kågedal Reimer <skagedal@gmail.com>
2022-12-28 10:31:10 +0100

    2015-04: Refactor

--- java/app/src/main/java/tech/skagedal/javaaoc/year2015/Day04.java
+++ java/app/src/main/java/tech/skagedal/javaaoc/year2015/Day04.java
@@ -1,5 +1,6 @@
 package tech.skagedal.javaaoc.year2015;
 
+import java.util.function.Predicate;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
 import tech.skagedal.javaaoc.aoc.AdventContext;
@@ -11,26 +12,19 @@ import tech.skagedal.javaaoc.aoc.AdventOfCodeRunner;
 )
 public class Day04 {
     public long part1(AdventContext context) {
-        final var digest = DigestUtils.getMd5Digest();
-        final var str = context.line();
-        for (var i = 0; i < Integer.MAX_VALUE; i++) {
-            String theString = str + i;
-            final var md5 = digest.digest(theString.getBytes());
-            if (hasFiveZeros(md5)) {
-                return i;
-            }
-        }
-
-        throw new IllegalStateException("no answer");
+        return findMd5Prefix(context.line(), Day04::hasFiveZeros);
     }
 
     public long part2(AdventContext context) {
+        return findMd5Prefix(context.line(), Day04::hasSixZeros);
+    }
+
+    private long findMd5Prefix(String line, Predicate<byte[]> stopPredicate) {
         final var digest = DigestUtils.getMd5Digest();
-        final var str = context.line();
         for (var i = 0; i < Integer.MAX_VALUE; i++) {
-            String theString = str + i;
+            String theString = line + i;
             final var md5 = digest.digest(theString.getBytes());
-            if (hasSixZeros(md5)) {
+            if (stopPredicate.test(md5)) {
                 return i;
             }
         }
@@ -47,8 +41,8 @@ public class Day04 {
     }
 
     public static void main(String[] args) {
-        AdventOfCodeRunner.example(new Day04(), "abcdef");
-        AdventOfCodeRunner.example(new Day04(), "pqrstuv");
+//        AdventOfCodeRunner.example(new Day04(), "abcdef");
+//        AdventOfCodeRunner.example(new Day04(), "pqrstuv");
         AdventOfCodeRunner.run(new Day04());
     }
 }
