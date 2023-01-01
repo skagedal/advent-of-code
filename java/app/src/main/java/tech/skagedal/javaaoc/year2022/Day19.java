c7a0f2219703d653865820800b818e19e221a2c4
Simon Kågedal Reimer <skagedal@gmail.com>
2023-01-01 18:29:52 +0100

    2022-19: try to think a bit

--- java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day19.java
+++ java/app/src/main/java/tech/skagedal/javaaoc/year2022/Day19.java
@@ -1,10 +1,8 @@
 package tech.skagedal.javaaoc.year2022;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import tech.skagedal.javaaoc.aoc.AdventContext;
 import tech.skagedal.javaaoc.aoc.AdventOfCode;
 import tech.skagedal.javaaoc.tools.streamsetc.Streams;
@@ -15,6 +13,26 @@ import tech.skagedal.javaaoc.tools.streamsetc.Streams;
 public class Day19 {
 
     public long part1(AdventContext context) {
+        return 0;
+    }
+
+    private long maxGeodes(Blueprint blueprint) {
+        return maxGeodes(
+            blueprint,
+            24,
+            Map.of(ResourceType.ORE, 1),
+            Map.of()
+        );
+    }
+
+    private long maxGeodes(Blueprint blueprint, int minutes, Map<ResourceType, Integer> machines, Map<ResourceType, Integer> resources) {
+        if (minutes == 0) {
+            return resources.getOrDefault(ResourceType.GEODE, 0);
+        }
+        // The options are:
+        //  1. buy nothing
+        //  2. buy a number of robots.
+        //     But should we support buying any number of robots? Or is it ok to just buy one at a time?
         return 0;
     }
 
@@ -29,7 +47,7 @@ public class Day19 {
             this.minutes = minutes;
         }
 
-        void run() {
+        long largestNumberOfGeodes() {
             // We get this one for free.
             deployMachines(List.of(new Machine(ResourceType.ORE, List.of())));
             for (var minute = 0; minute < minutes; minute++) {
@@ -38,14 +56,46 @@ public class Day19 {
                 collectProduction();
                 deployMachines(boughtMachines);
             }
+            return resources.getOrDefault(ResourceType.GEODE, 0);
         }
 
         private List<Machine> buyMachines() {
-            // This one is the tricky one.
+            // This one is the tricky one, the actual problem to solve. I'm not sure how.
+            // Gonna just play with some stupid greedy algorithm.
             for (var resourceType : ResourceType.BY_PRIORITY) {
-
+                final var machine = findMachine(resourceType);
+                if (canAfford(machine.price())) {
+                    withdraw(machine.price());
+                    return List.of(machine);
+                }
             }
+
+            // Clearly not the thing to do.
+
             return List.of();
+        }
+
+        private Machine findMachine(ResourceType resourceType) {
+            return blueprint.machines().stream()
+                .filter(m -> m.resourceType.equals(resourceType)).findFirst().orElseThrow();
+        }
+
+        private boolean canAfford(List<Price> price) {
+            return price.stream().allMatch(p -> resourceCount(p.resourceType()) >= p.amount());
+        }
+
+        void withdraw(List<Price> price) {
+            for (var p : price) {
+                withdrawResource(p.amount(), p.resourceType());
+            }
+        }
+
+        void withdrawResource(int amount, ResourceType resourceType) {
+            resources.put(resourceType, resources.getOrDefault(resourceType, 0) - amount);
+        }
+
+        private int resourceCount(ResourceType resourceType) {
+            return resources.getOrDefault(resourceType, 0);
         }
 
         private void collectProduction() {
@@ -91,12 +141,18 @@ public class Day19 {
             Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
             """;
 
-        new Simulation(
+        final var lnog = new Simulation(
             new Blueprint(1, List.of(
-
+                new Machine(ResourceType.ORE, List.of(new Price(4, ResourceType.ORE))),
+                new Machine(ResourceType.CLAY, List.of(new Price(2, ResourceType.ORE))),
+                new Machine(ResourceType.OBSIDIAN, List.of(new Price(3, ResourceType.ORE), new Price(14, ResourceType.CLAY))),
+                new Machine(ResourceType.GEODE, List.of(new Price(2, ResourceType.ORE), new Price(7, ResourceType.OBSIDIAN)))
             )),
             24
-        ).run();
+        ).largestNumberOfGeodes();
+
+        // A geode robot costs 2 ore and 7 obsidian.
+        // As soon as we have that, we should make sure to by a geode robot.
     }
 
 }
