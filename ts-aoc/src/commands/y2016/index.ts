96b74c87b5772066415e5821b0cdd6c605a06114
Simon Kågedal Reimer <skagedal@gmail.com>
2023-10-28 22:16:45 +0200

    Add skeleton y2016 command

---
+++ ts-aoc/src/commands/y2016/index.ts
@@ -0,0 +1,10 @@
+import { Command } from "@oclif/core";
+
+export default class Y2016 extends Command {
+    static description = 'Advent of Code 2016'
+
+    async run(): Promise<void> {
+        console.log('Advent of Code 2016');
+    }
+
+}
