676beb5fb5630d92a2503b3a14bd8b401bb57f1a
Simon Kågedal Reimer <skagedal@gmail.com>
2023-12-08 09:17:22 +0100

    2023-07: wip refactor

--- java/src/main/java/tech/skagedal/javaaoc/year2023/Day07.java
+++ java/src/main/java/tech/skagedal/javaaoc/year2023/Day07.java
@@ -7,6 +7,7 @@ import tech.skagedal.javaaoc.tools.streamsetc.Streams;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 @AdventOfCode(
@@ -29,6 +30,66 @@ public class Day07 {
             .sum();
     }
 
+    record HandClassifier(HandType handType, List<Pattern> pattern, Map<Integer, HandType> joker) {}
+
+    static List<HandClassifier> classifiers = List.of(
+        new HandClassifier(
+            HandType.FIVE_OF_A_KIND,
+            List.of(Pattern.compile("(.)\\1{4}")),
+            Map.of()
+        ),
+        new HandClassifier(
+            HandType.FOUR_OF_A_KIND,
+            List.of(Pattern.compile("(.)\\1{3}")),
+            Map.of(
+                1, HandType.FIVE_OF_A_KIND,
+                4, HandType.FIVE_OF_A_KIND
+            )
+        ),
+        new HandClassifier(
+            HandType.FULL_HOUSE,
+            List.of(
+                Pattern.compile("(.)\\1{2}(.)\\2"),
+                Pattern.compile("(.)\\1(.)\\2{2}")
+            ),
+            Map.of(
+                2, HandType.FIVE_OF_A_KIND,
+                3, HandType.FIVE_OF_A_KIND
+            )
+        ),
+        new HandClassifier(
+            HandType.THREE_OF_A_KIND,
+            List.of(Pattern.compile("(.)\\1{2}")),
+            Map.of(
+                1, HandType.FOUR_OF_A_KIND,
+                3, HandType.FOUR_OF_A_KIND
+            )
+        ),
+        new HandClassifier(
+            HandType.TWO_PAIR,
+            List.of(Pattern.compile("(.)\\1.*(.)\\2")),
+            Map.of(
+                1, HandType.FULL_HOUSE,
+                2, HandType.FOUR_OF_A_KIND
+            )
+        ),
+        new HandClassifier(
+            HandType.ONE_PAIR,
+            List.of(Pattern.compile("(.)\\1")),
+            Map.of(
+                1, HandType.THREE_OF_A_KIND,
+                2, HandType.THREE_OF_A_KIND
+            )
+        ),
+        new HandClassifier(
+            HandType.HIGH_CARD,
+            List.of(Pattern.compile(".")),
+            Map.of(
+                1, HandType.ONE_PAIR
+            )
+        )
+    );
+
     public record BidHand(
         String hand,
         long bid
@@ -41,16 +102,6 @@ public class Day07 {
             );
         }
 
-        static Pattern FIVE_OF_A_KIND = Pattern.compile("(.)\\1{4}");
-        static Pattern FOUR_OF_A_KIND = Pattern.compile("(.)\\1{3}");
-        static List<Pattern> FULL_HOUSE = List.of(
-            Pattern.compile("(.)\\1{2}(.)\\2"),
-            Pattern.compile("(.)\\1(.)\\2{2}")
-        );
-        static Pattern THREE_OF_A_KIND = Pattern.compile("(.)\\1{2}");
-        static Pattern TWO_PAIR = Pattern.compile("(.)\\1.*(.)\\2");
-        static Pattern ONE_PAIR = Pattern.compile("(.)\\1");
-
         public static Comparator<BidHand> comparator(boolean useJokers, String rankOrdering) {
             return Comparator
                 .comparingLong((BidHand bidHand) -> bidHand.typeValue(useJokers))
@@ -61,50 +112,13 @@ public class Day07 {
             var sortedHand = hand.codePoints().sorted().collect(StringBuilder::new,
                 StringBuilder::appendCodePoint,
                 StringBuilder::append);
+
             var jokerCount = useJokers ? hand.codePoints().filter(c -> c == 'J').count() : 0;
-            if (FIVE_OF_A_KIND.matcher(sortedHand).find()) {
-                return HandType.FIVE_OF_A_KIND;
-            }
-            if (FOUR_OF_A_KIND.matcher(sortedHand).find()) {
-                if (jokerCount == 1 || jokerCount == 4) {
-                    return HandType.FIVE_OF_A_KIND;
-                }
-                return HandType.FOUR_OF_A_KIND;
-            }
-            if (FULL_HOUSE.stream().anyMatch(p -> p.matcher(sortedHand).find())) {
-                if (jokerCount == 2 || jokerCount == 3) {
-                    return HandType.FIVE_OF_A_KIND;
-                }
-                return HandType.FULL_HOUSE;
-            }
-            if (THREE_OF_A_KIND.matcher(sortedHand).find()) {
-                if (jokerCount == 1 || jokerCount == 3) {
-                    return HandType.FOUR_OF_A_KIND;
-                }
-                if (jokerCount == 2) {
-                    return HandType.FIVE_OF_A_KIND;
-                }
-                return HandType.THREE_OF_A_KIND;
-            }
-            if (TWO_PAIR.matcher(sortedHand).find()) {
-                if (jokerCount == 1) {
-                    return HandType.FULL_HOUSE;
-                }
-                if (jokerCount == 2) {
-                    return HandType.FOUR_OF_A_KIND;
-                }
-                return HandType.TWO_PAIR;
-            }
-            if (ONE_PAIR.matcher(sortedHand).find()) {
-                if (jokerCount == 1 || jokerCount == 2) {
-                    return HandType.THREE_OF_A_KIND;
-                }
-                return HandType.ONE_PAIR;
-            }
-            if (jokerCount == 1) {
-                return HandType.ONE_PAIR;
-            }
-            return HandType.HIGH_CARD;
+
+            classifiers.stream()
+                .findFirst()
+                .map(HandClassifier::handType)
+
         }
 
         public long typeValue(boolean useJokers) {
