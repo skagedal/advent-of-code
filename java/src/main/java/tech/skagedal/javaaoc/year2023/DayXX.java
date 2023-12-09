4adfcd45a1a3d16f9900b6a07500640ee1697729
Simon Kågedal Reimer <skagedal@gmail.com>
2023-12-09 14:42:33 +0100

    add template for days

---
+++ java/src/main/java/tech/skagedal/javaaoc/year2023/DayXX.java
@@ -0,0 +1,23 @@
+package tech.skagedal.javaaoc.year2023;
+
+import tech.skagedal.javaaoc.aoc.AdventContext;
+import tech.skagedal.javaaoc.aoc.AdventOfCode;
+import tech.skagedal.javaaoc.aoc.AdventOfCodeRunner;
+
+@AdventOfCode(
+    description = ""
+)
+public class DayXX {
+    public long part1(AdventContext context) {
+        return 0;
+    }
+
+    public long part2(AdventContext context) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        AdventOfCodeRunner.example(new DayXX(), """
+            """);
+    }
+}
