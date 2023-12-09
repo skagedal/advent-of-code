691933681a66ac750075dfb547a4d2aa133c6b4e
Simon Kågedal Reimer <skagedal@gmail.com>
2023-12-09 14:42:27 +0100

    2023-08: solve part 2

---
+++ java/src/main/java/tech/skagedal/javaaoc/tools/math/Maths.java
@@ -0,0 +1,18 @@
+package tech.skagedal.javaaoc.tools.math;
+
+public class Maths {
+    public static long lcm(long number1, long number2) {
+        if (number1 == 0 || number2 == 0) {
+            return 0;
+        }
+        long absNumber1 = Math.abs(number1);
+        long absNumber2 = Math.abs(number2);
+        long absHigherNumber = Math.max(absNumber1, absNumber2);
+        long absLowerNumber = Math.min(absNumber1, absNumber2);
+        long lcm = absHigherNumber;
+        while (lcm % absLowerNumber != 0) {
+            lcm += absHigherNumber;
+        }
+        return lcm;
+    }
+}
--- java/src/main/java/tech/skagedal/javaaoc/year2023/Day08.java
+++ java/src/main/java/tech/skagedal/javaaoc/year2023/Day08.java
@@ -1,14 +1,17 @@
 package tech.skagedal.javaaoc.year2023;
 
+import org.springframework.context.event.SourceFilteringListener;
 import tech.skagedal.javaaoc.aoc.AdventContext;
 import tech.skagedal.javaaoc.aoc.AdventOfCode;
 import tech.skagedal.javaaoc.aoc.AdventOfCodeRunner;
 import tech.skagedal.javaaoc.tools.function.Tuple2;
 import tech.skagedal.javaaoc.tools.function.Tuple3;
+import tech.skagedal.javaaoc.tools.math.Maths;
 import tech.skagedal.javaaoc.tools.regex.Patterns;
+import tech.skagedal.javaaoc.tools.streamsetc.Enumerated;
 import tech.skagedal.javaaoc.tools.streamsetc.Streams;
 
-import java.util.List;
+import java.util.*;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -21,7 +24,7 @@ public class Day08 {
         var sections = Streams.splitting(context.lines(), String::isBlank).toList();
         var instructions = sections.getFirst().getFirst();
         var nodeMap = sections.getLast();
-        var nodes = nodeMap.stream().map(Day08::edgeDescription).collect(Collectors.toMap(
+        var edges = nodeMap.stream().map(Day08::edgeDescription).collect(Collectors.toMap(
             List::getFirst,
             list -> new Tuple2<>(list.get(1), list.get(2))
         ));
@@ -30,7 +33,7 @@ public class Day08 {
         var node = "AAA";
         while (!node.equals("ZZZ")) {
             var instruction = instructions.charAt((int)steps % (instructions.length()));
-            var edge = nodes.get(node);
+            var edge = edges.get(node);
             node = switch (instruction) {
                 case 'L' -> edge.value1();
                 case 'R' -> edge.value2();
@@ -42,32 +45,95 @@ public class Day08 {
     }
 
     public long part2(AdventContext context) {
-        return 0;
+        var sections = Streams.splitting(context.lines(), String::isBlank).toList();
+        var instructions = sections.getFirst().getFirst();
+        var nodeMap = sections.getLast();
+        var edges = nodeMap.stream().map(Day08::edgeDescription).collect(Collectors.toMap(
+            List::getFirst,
+            list -> new Tuple2<>(list.get(1), list.get(2))
+        ));
+
+        System.out.printf("Number of instructions: %d\n", instructions.length());
+        System.out.printf("Number of edges: %d\n", edges.keySet().size());
+
+        long steps = 0;
+        var currentNodes = findStartNodes(edges);
+        System.out.printf("Start nodes: %d\n", currentNodes.length);
+
+        var finder = new Finder(instructions, edges);
+        for (var i = 0; i < currentNodes.length; i++) {
+            finder.findLoop(currentNodes[i]);
+        }
+
+        var endStates = Arrays.stream(currentNodes).mapToInt(finder::findLoop).toArray();
+        System.out.printf("LCD of %s\n", Arrays.stream(endStates).mapToObj(String::valueOf).collect(Collectors.joining(", ")));
+
+        long s = endStates[0];
+        for (var i = 1; i < endStates.length; i++) {
+            s = Maths.lcm(s, endStates[i]);
+        }
+        return s;
     }
 
-    static Pattern NODE_PATTERN = Pattern.compile("[A-Z]{3}");
+    static class Finder {
+        private final String instructions;
+        private final Map<String, Tuple2<String, String>> edges;
+
+        record State(int instruction, String node) {
+            boolean isEndNode() {
+                return node.endsWith("Z");
+            }
+        }
+        Finder(String instructions, Map<String, Tuple2<String, String>> edges) {
+            this.instructions = instructions;
+            this.edges = edges;
+        }
+
+        int findLoop(String node) {
+            int loopLength = 0;
+            var seenStates = new HashMap<State, Integer>();
+            var seenStatesList = new ArrayList<State>();
+            var current = new State(0, node);
+            while (!seenStates.containsKey(current)) {
+                seenStates.put(current, loopLength);
+                seenStatesList.add(current);
+                var instructionCharacter = instructions.charAt(current.instruction);
+                var edge = edges.get(current.node);
+                var nextNode = switch (instructionCharacter) {
+                    case 'L' -> edge.value1();
+                    case 'R' -> edge.value2();
+                    default -> throw new IllegalArgumentException();
+                };
+                current = new State((current.instruction + 1) % instructions.length(), nextNode);
+                loopLength += 1;
+            }
+
+            System.out.printf(" - Loop starting with %s has length %d, ends with %s\n", seenStatesList.getFirst(), seenStatesList.size(), seenStatesList.getLast());
+            System.out.printf("   Then it starts over with %s.\n", current);
+            final var realLoopLength = seenStatesList.size() - current.instruction();
+            System.out.printf("   So, the loop length is %d.\n", realLoopLength);
+            System.out.printf("   We find end states at " + Streams.enumerated(seenStatesList.stream()).filter(es -> es.value().isEndNode()).map(Enumerated::number).map(String::valueOf).collect(Collectors.joining(", ")) + "\n");
+            final var endState = Streams.enumerated(seenStatesList.stream()).filter(es -> es.value().isEndNode()).mapToInt(Enumerated::number).findAny().orElseThrow();
+            System.out.printf("   Assume only one. It's at %d.\n", endState);
+            System.out.printf("   So, we will have end states at every i = %d + (%d * k)\n", endState, realLoopLength);
+            return endState;
+        }
+    }
+
+    private String[] findStartNodes(Map<String, Tuple2<String, String>> edges) {
+        return edges.keySet().stream().filter(s -> s.endsWith("A")).toArray(String[]::new);
+    }
+
+    private boolean isEndNodes(String[] currentNodes) {
+        return Arrays.stream(currentNodes).allMatch(s -> s.endsWith("Z"));
+    }
+
+    static Pattern NODE_PATTERN = Pattern.compile("[A-Z0-9]{3}");
     private static List<String> edgeDescription(String s) {
         return NODE_PATTERN.matcher(s).results().map(MatchResult::group).toList();
     }
 
     public static void main(String[] args) {
-        AdventOfCodeRunner.example(new Day08(), """
-            RL
-                        
-            AAA = (BBB, CCC)
-            BBB = (DDD, EEE)
-            CCC = (ZZZ, GGG)
-            DDD = (DDD, DDD)
-            EEE = (EEE, EEE)
-            GGG = (GGG, GGG)
-            ZZZ = (ZZZ, ZZZ)""");
-
-        AdventOfCodeRunner.example(new Day08(), """
-            LLR
-                        
-            AAA = (BBB, BBB)
-            BBB = (AAA, ZZZ)
-            ZZZ = (ZZZ, ZZZ)""");
         AdventOfCodeRunner.run(new Day08());
     }
 }
